118af9051b8691d364d252a0a9a2359dde6e3c7e
Mees Dekker <meesdekker2622002@gmail.com>
2022-11-20 16:12:54 +0100

    memmove test for kenny

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,11 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/06 16:07:20 by mdekker       #+#    #+#                 */
-/*   Updated: 2022/11/20 03:05:48 by mdekker       ########   odam.nl         */
+/*   Updated: 2022/11/20 16:04:07 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 void	*loop(char *dst, char *src, size_t len, size_t dir)
 {
@@ -23,6 +21,8 @@ void	*loop(char *dst, char *src, size_t len, size_t dir)
 	{
 		while (i < len)
 		{
+			printf("src1: %d\n", src[i]);
+			// printf("i: %zu dst: %p src: %p\n", i, dst, src);
 			dst[i] = src[i];
 			i++;
 		}
@@ -32,8 +32,10 @@ void	*loop(char *dst, char *src, size_t len, size_t dir)
 		i = len;
 		while (i > 0)
 		{
-			i--;
+			printf("src2: %d\n", src[i]);
+			// printf("i: %zu dst: %p src: %p\n", i, dst, src);
 			dst[i] = src[i];
+			i--;
 		}
 	}
 	return (dst);
@@ -41,14 +43,27 @@ void	*loop(char *dst, char *src, size_t len, size_t dir)
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*to;
 	char	*from;
+	char	*to;
 
 	to = (char *)dst;
 	from = (char *)src;
 	if (to < from)
-		loop(to, from, len, 0);
+		printf("this is the loop return1:	%s\n", (char *)loop(to, from, len,
+						0));
 	else if (to > from)
-		loop(to, from, len, 1);
+		printf("this is the loop return2:	%s\n", (char *)loop(to, from, len,
+						1));
 	return (dst);
+}
+
+int	main(void)
+{
+	char str[] = "1234567890";
+	char	*str2;
+
+
+	str2 = (char *)malloc(sizeof(char) * 10);
+	ft_memmove(str2, str, 10);
+	printf("\nthis is main str:	'%s'\n", str2);
 }
